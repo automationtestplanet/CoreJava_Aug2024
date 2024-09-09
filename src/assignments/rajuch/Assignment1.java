bd6338b012a1a023881ec8a433708487a3f47d5b
RAJU CHELLE <rajuchelle@gmail.com>
2024-09-09 08:56:25 +0530

    new assignments

---
+++ src/assignments/rajuch/Assignment1.java
@@ -0,0 +1,18 @@
+package assignments.rajuch;
+
+public class Assignment1 {
+
+	
+	// write a reusable code to extract numbers from string and make the sum of digits
+	// Ex : String str = "Abcd1234%#&56KLLCcsk" 
+	// o/p: 1+2+3+4+5+6 -> 21
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
