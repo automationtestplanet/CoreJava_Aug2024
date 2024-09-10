b9da81d6444e1a83e5ca5918b83087d19309941f
Sangita Parida <sp.sangitaparida@gmail.com>
2024-09-10 08:28:24 +0530

    Sangita Assignment1

--- src/assignments/sangita/Assignment1.java
+++ src/assignments/sangita/Assignment1.java
@@ -1,12 +1,33 @@
 package assignments.sangita;
 
+import java.util.Arrays;
+
 public class Assignment1 {
 	
 	// Write a reusable program to check the given tow Strings/numbers are anagram or not 
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	public static boolean areAnagram(String s1, String s2)
+    {
+        String str1= s1.replaceAll("\\s", "");
+        String str2= s2.replaceAll("\\s", "");
+        
+        if(str1.length() != str2.length()) 
+        	{
+        	return false;
+            } 	
+        else {
+        	char c1[]= str1.toLowerCase().toCharArray();
+        	char c2[]= str1.toLowerCase().toCharArray();
+          	// Sort both strings
+            Arrays.sort(c1);
+            Arrays.sort(c2);
+            return Arrays.equals(c1,c2);
+            }
+       }
+  	  public static void main(String args[])
+      {
+       System.out.println (areAnagram("Listen","Silent"));
+         
+  	  }
 }
+
