ef44fb2a298cd16d4d72f532cf850de7b60c79b8
Mounika <reddimounica6@gmail.com>
2024-09-12 14:55:40 +0530

    Assignment1 by mounika

--- src/assignments/mounika/Assignment1.java
+++ src/assignments/mounika/Assignment1.java
@@ -1,14 +1,49 @@
 package assignments.mounika;
 
 public class Assignment1 {
-	
-	// Write a reusable program to print given numbers sum of digits is a prime number or not
+
+	// Write a reusable program to print given numbers sum of digits is a prime
+	// number or not
 	// Ex: num = 123 = 6 -> not prime
-	//  num = 124  = 7 -> prime 
+	// num = 124 = 7 -> prime
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static boolean isPrime(int number) {
+		if (number <= 1) {
+			return false;
+		}
+		if (number == 2) {
+			return true;
+		}
+		if (number % 2 == 0) {
+			return false;
+		}
+		for (int i = 3; i * i <= number; i = +2) {
+			if (number % i == 0) {
+				return false;
+			}
+		}
+		return true;
 	}
 
+	public static int sumOfDigits(int number) {
+		int sum = 0;
+		while (number > 0) {
+			sum = sum + number % 10;
+			number = number / 10;
+		}
+		return sum;
+	}
+
+	public static void printSumOfDigitsOfPrimeOrNot(int number) {
+		int digitSum = sumOfDigits(number);
+		boolean isPrimeSum = isPrime(digitSum);
+		System.out.println("num = " + number + " = " + digitSum + " -> " + (isPrimeSum ? "prime" : "not prime"));
+	}
+
+	public static void main(String[] args) {
+		int[] numbers = { 123, 124, 13 };
+		for (int number : numbers) {
+			printSumOfDigitsOfPrimeOrNot(number);
+		}
+	}
 }
